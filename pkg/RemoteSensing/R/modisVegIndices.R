048d575e4e2938aee796f461e2879fdd30cfeb36
jaunario (Jorrel Khalil Aunario) <jaunario@gmail.com>
2010-02-18 10:44:16 +0000

    Optimized modisVeg function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remotesensing@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteSensing/R/modisVegIndices.R
+++ pkg/RemoteSensing/R/modisVegIndices.R
@@ -1,4 +1,4 @@
-# Authors: Sonia Asilo, Ritsuko Fuchiyama, Robert J. Hijmans, Yann Chemin, Angelo Carlo Pacheco
+# Authors: Sonia Asilo, Ritsuko Fuchiyama, Robert J. Hijmans, Yann Chemin, Angelo Carlo Pacheco, Jorrel Khalil S. Aunario
 # International Rice Research Institute
 # Date :  Feb 2009
 # Version 0,1
@@ -26,77 +26,87 @@ modisVeg <- function(inpath, tileNumber="0"){
 		m <- modisFilesClean(inpath, pat)
 
 		#creation of output director "tif" folder
-		outpth <- paste(inpath,"/../veg/",sep="")
-		dir.create(outpth, showWarnings = FALSE)
+		outpath <- paste(inpath,"/../veg",sep="")
+		if (!file.exists(outpath)) dir.create(outpath, recursive=TRUE)
 
 		# looping
-		zones <- as.vector(unique(m$zone))
+		zones <- unique(m$zone)
 		for (z in zones) {
 			mm <- subset(m, m$zone==z)
-			dates <- as.vector(unique(mm$date))
-			print(paste('Zone:', z))
+			dates <- unique(mm$date)
+			cat("Zone:", z, "\n")
+			flush.console()
 
 			for (d in dates) {
-				print(d)
+			    dlab <- paste("Date ", d, ":", sep ="")
+				cat(dlab, "loading images. \r")
+				flush.console()
 				b <- subset(mm, mm$date == d)
-				fname <- paste(outpth, b$date[1], '_', b$zone[1], '_', sep='')
-				for (i in 1:length(b[,1])) { 
-					if (b$band[i] == 'b01') {
-						red <- raster(paste(inpath, b$filename[i], sep=''), FALSE )
-					} else if (b$band[i] == 'b02') {
-						nir <- raster(paste(inpath, b$filename[i], sep=''), FALSE )
-					} else if (b$band[i] == 'b03') {
-						blue <- raster(paste(inpath, b$filename[i], sep=''), FALSE )
-					} else if (b$band[i] == 'b04') {
-						green <- raster(paste(inpath, b$filename[i], sep=''), FALSE)
-					} else if (b$band[i] == 'b06') {
-						swir1 <- raster(paste(inpath, b$filename[i], sep=''), FALSE)
-					} else if (b$band[i] == 'b07') {
-						swir2 <- raster(paste(inpath, b$filename[i], sep=''), FALSE)
-					}
-				}
-				# making of VIs
-				NDVI <- overlay(red, nir, fun=ndvi, filename=paste(fname, 'ndvi.grd', sep=''), overwrite=TRUE)
-				LSWI <- overlay(nir, swir1, fun=lswi,  filename=paste(fname, 'lswi.grd', sep=''), overwrite=TRUE)
-				EVI <- overlay(blue, red, nir, fun=evi, filename=paste(fname, 'evi.grd', sep=''), overwrite=TRUE)
-				NDWI <- overlay(nir, swir2, fun=ndwi, filename=paste(fname, 'ndwi.grd', sep=''), overwrite=TRUE)
+				fname <- paste(outpath, "/", b$date[1], "_", b$zone[1], "_", sep="")
+				
+                red <- raster(paste(inpath, b$filename[b$band=="b01"], sep="/"), values=TRUE)
+				nir <- raster(paste(inpath, b$filename[b$band=="b02"], sep="/"), values=TRUE)
+				blue <- raster(paste(inpath, b$filename[b$band=="b03"], sep="/"), values=TRUE)
+				green <- raster(paste(inpath, b$filename[b$band=="b04"], sep="/"), values=TRUE)
+				swir1 <- raster(paste(inpath, b$filename[b$band=="b06"], sep="/"), values=TRUE)
+				swir2 <- raster(paste(inpath, b$filename[b$band=="b07"], sep="/"), values=TRUE)
+				
+                # making of VIs
+                cat (dlab, "computing vegetation indices. \r")
+                flush.console()
+				NDVI <- overlay(red, nir, fun=ndvi)
+				LSWI <- overlay(nir, swir1, fun=lswi)
+				EVI <- overlay(blue, red, nir, fun=evi)
+				NDWI <- overlay(nir, swir2, fun=ndwi)
 				
 				# masking of VIs
-				pat1 <- paste(d, "_", z, "_b03_mask.grd", sep="")
-				bluemaskfiles <- list.files(inpath, pattern=pat1)
-				bluemask <- raster(paste(inpath, bluemaskfiles[1], sep=""))
+				cat (dlab, "masking vegetation indices.   \r")
+                flush.console()
 				
-				pat2 <- paste(d, "_", z, "_SnowMask2.grd", sep="")
-				snowmaskfiles <- list.files(inpath, pattern=pat2)
-				snowmask <- raster(paste(inpath, snowmaskfiles[1], sep=""))
+				#pat1 <- paste(d, "_", z, "_b03_mask.grd", sep="") 
+				bluemaskfile <- paste(inpath, paste(d, "_", z, "_b03_mask.grd", sep=""), sep="/")
+				if(!file.exists(bluemaskfile)) stop(paste(bluemaskfile, "does not exist!"))
+				bluemask <- raster(bluemaskfile, values=TRUE)
 				
-				NDVI <- overlay(NDVI, bluemask, snowmask, fun=multiply, filename=paste(fname, 'ndvi-cleaned.grd', sep=''), overwrite=TRUE)
-				LSWI <- overlay(LSWI, bluemask, snowmask, fun=multiply,  filename=paste(fname, 'lswi-cleaned.grd', sep=''), overwrite=TRUE)
-				EVI <- overlay(EVI, bluemask, snowmask, fun=multiply, filename=paste(fname, 'evi-cleaned.grd', sep=''), overwrite=TRUE)
-				NDWI <- overlay(NDWI, bluemask, snowmask, fun=multiply, filename=paste(fname, 'ndwi-cleaned.grd', sep=''), overwrite=TRUE)
-				NDDI <- overlay(NDVI, NDWI, fun=nddi, filename=paste(fname, 'nddi-cleaned.grd', sep=''), overwrite=TRUE)
+				#pat2 <- paste(d, "_", z, "_SnowMask2.grd", sep="")
+				#snowmaskfiles <- list.files(inpath, pattern=pat2)
+				snowmaskfile <- paste(inpath, paste(d, "_", z, "_SnowMask2.grd", sep=""), sep="/")
+				if(!file.exists(snowmaskfile)) stop(paste(snowmaskfile, "does not exist!"))
+				snowmask <- raster(snowmaskfile, values=TRUE)
+				
+				
+				NDVI <- overlay(NDVI, bluemask, snowmask, fun=multiply, filename=paste(fname, "ndvi-cleaned.grd", sep=""), overwrite=TRUE)
+				LSWI <- overlay(LSWI, bluemask, snowmask, fun=multiply,  filename=paste(fname, "lswi-cleaned.grd", sep=""), overwrite=TRUE)
+				EVI <- overlay(EVI, bluemask, snowmask, fun=multiply, filename=paste(fname, "evi-cleaned.grd", sep=""), overwrite=TRUE)
+				NDWI <- overlay(NDWI, bluemask, snowmask, fun=multiply, filename=paste(fname, "ndwi-cleaned.grd", sep=""), overwrite=TRUE)
+				NDDI <- overlay(NDVI, NDWI, fun=nddi, filename=paste(fname, "nddi-cleaned.grd", sep=""), overwrite=TRUE)
 								
 				# writing of flooded,permanent water and drought
-				flood <- overlay(LSWI, NDVI, EVI, fun=flooded, filename=paste(fname, 'flooded.grd', sep=''), overwrite=TRUE,  datatype='INT2S')
-				permanent <- overlay(NDVI, LSWI, fun=persistentwater, filename=paste(fname, 'permanent.grd', sep=''), overwrite=TRUE, datatype='INT2S')
-				droughtpix <- overlay(NDVI, NDWI, fun=.drought, filename=paste(fname, 'drought.grd', sep=''), overwrite=TRUE, datatype='INT2S')
+				cat (dlab, "Computing drought, flooded, and permanent water \r")
+                flush.console()				
+				flood <- overlay(LSWI, NDVI, EVI, fun=flooded, filename=paste(fname, "flooded.grd", sep=""), overwrite=TRUE,  datatype="INT1S")
+				permanent <- overlay(NDVI, LSWI, fun=persistentwater, filename=paste(fname, "permanent.grd", sep=""), overwrite=TRUE, datatype="INT1S")
+				droughtpix <- overlay(NDVI, NDWI, fun=.drought, filename=paste(fname, 'drought.grd', sep=''), overwrite=TRUE, datatype='INT1S')
+                cat (dlab, " -------------------- DONE -------------------- \n")
+                flush.console()
+				
+				
 			}
 		}
 	}
 	# processing of all tiles in a directory
 	if(tileNumber=="0"){
-		print("You did not indicate a tile number. The script will process all the existing tiles in the inpath...")
-		str <- list.files(inpath, pattern="001.*b01")
+		cat("You did not indicate a tile number. The script will process all the existing tiles in the inpath... \n")
+		flush.console()
+		str <- list.files(inpath, pattern="001.*b01.*grd")
 		str2 <- substr(str, 10, 15)
 		for(i in str2){
-			print(paste("Now mapping tile:", i ))
+			# print(paste("Now mapping tile:", i ))
 			vegFxn(inpath, i)
 		}
 	}
 	# Processing of only one tile indicated in the function parameter
 	else{
 		vegFxn(inpath, tileNumber)
-	}
-
-	
+	}	
 }
--- pkg/RemoteSensing/R/water.R
+++ pkg/RemoteSensing/R/water.R
@@ -1,4 +1,4 @@
-# Author: Yann Chemin & Aileen Maunahan
+# Author: Yann Chemin & Aileen Maunahan, Sonia Asilo, Jorrel Khalil S. Aunario
 # IRRI
 # License GPL3
 # Version 2, March 2009
@@ -43,15 +43,18 @@ flooded <- function(lswi, ndvi, evi) {
 #Xiao X., Boles S., Liu J., Zhuang D., Frokling S., Li C., Salas W., Moore III B. (2005). 
  #Mapping paddy rice agriculture in southern China using multi-temporal MODIS images. 
  #Remote Sensing of Environment 95:480-492.
- 	return( (lswi+0.05 >= evi) | (lswi+0.05 >= ndvi) )
+    res <- rep(0, length(lswi))
+    res[(lswi+0.05 >= evi) | (lswi+0.05 >= ndvi)] <- 1
+    return(res)
 }
 
 
 persistentwater <- function(ndvi,lswi){ 
 #Xiao X., Boles S., Liu J., Zhuang D., Frokling S., Li C., Salas W., Moore III B. (2005). 
 #Mapping paddy rice agriculture in southern China using multi-temporal MODIS images. 
-#Remote Sensing of Environment 95:480-492.	
-		result <- (ndvi < 0.10) & (ndvi < lswi)
-		return(result)
-		}
+#Remote Sensing of Environment 95:480-492.
+    res <- rep(0, length(ndvi))
+    res[(ndvi < 0.10) & (ndvi < lswi)] <- 1	
+	return(res)
+}
 

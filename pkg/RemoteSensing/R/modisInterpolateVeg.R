4c75f4a00f0d391563f8c214acde1d6653799c42
sonia (Ms. ) <slasilo@gmail.com>
2010-06-17 06:46:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remotesensing@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteSensing/R/modisInterpolateVeg.R
+++ pkg/RemoteSensing/R/modisInterpolateVeg.R
@@ -16,12 +16,12 @@ modisInterpolateVeg <- function(inpath, informat="raster"){
     for (i in 1:length(indnames)){
         files <- cbind(files, list.files(inpath, pattern=paste(indnames[i],".cleaned",ext,sep="")))
         stck <- stack(paste(inpath,files[,i],sep="/"))
-        interp <- tsInterpolate(evistack, targetfolder=interppath)
+        interp <- tsInterpolate(stck, targetfolder=interppath)
         interp <- writeRaster(interp,paste(interppath,paste(indnames[i],"_na_count.grd",sep=""), sep="/"), datatype="INT1U")
-        rm(interp,stck)
+        rm(interp,stck,files)
         gc(verbose=FALSE)    
     }
-   
+	files <- cbind(list.files(interppath, pattern="evi.cleaned.tif"),list.files(interppath, pattern="ndvi.cleaned.tif"),list.files(interppath, pattern="lswi.cleaned.tif"),list.files(interppath, pattern="ndwi.cleaned.tif"))
     for (i in 1:nrow(files)){
         base <- substring(files[i,1],1,16)
         cat(base, "\r")
@@ -30,25 +30,31 @@ modisInterpolateVeg <- function(inpath, informat="raster"){
         for(j in 1:ncol(files)){
             ras <- raster(paste(interppath,files[i,j],sep="/"))
             NAvalue(ras) <- -9999
-            indices[[indnames[j]]] <- getValues(ras)
+            indices[[indnames[j]]] <- getValues(ras)/10000
         }
-        indices <- as.data.frame(indices)/10000
+        
+		
         flood <- flooded(indices$lswi,indices$ndvi,indices$evi)
         fld <- raster2SGDF(ras,vals=flood)
-        fld <- writeGDAL(fld,paste(interppath,paste(base,"flooded.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
+        writeGDAL(fld,paste(interppath,paste(base,"flooded.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
+		rm(flood,fld)
+		gc(verbose=FALSE)
 		permanent <- persistentwater(indices$ndvi,indices$lswi)
 		perm <- raster2SGDF(ras,vals=permanent)
-		perm <- writeGDAL(perm,paste(interppath,paste(base,"permanent.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
-		nddi <- flooded(indices$lswi,indices$ndvi,indices$evi)
+		writeGDAL(perm,paste(interppath,paste(base,"permanent.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
+		rm(permanent,perm)
+		gc(verbose=FALSE)
+		nddi <- nddi(indices$ndvi,indices$ndwi)
         nd <- raster2SGDF(ras,vals=nddi)
-        nd <- writeGDAL(nd,paste(interppath,paste(base,"nddi.cleaned.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Float32")
-		drought <- persistentwater(indices$ndvi,indices$lswi)
-		drt <- raster2SGDF(ras,vals=drought)
-		drt <- writeGDAL(drt,paste(interppath,paste(base,"drought.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
-        
-        rm(indices,flood, fld, permanent, perm)
-        rm(indices,nddi, nd, drought, drt)
-        gc(verbose=FALSE)			
-    }
+        writeGDAL(nd,paste(interppath,paste(base,"nddi.cleaned.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Float32")
+		rm(nddi,nd)
+		gc(verbose=FALSE)
+		
+		drght <- drought(indices$ndvi,indices$ndwi)
+		drt <- raster2SGDF(ras,vals=drght)
+		writeGDAL(drt,paste(interppath,paste(base,"drought.tif",sep=""),sep="/"), options=c("COMPRESS=LZW", "TFW=YES"), type="Int16")
+        rm(drght,drt,indices)
+		gc(verbose=FALSE)		
+        }
 
 }

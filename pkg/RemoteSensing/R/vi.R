14f32bc29a76fc220e4e10dee4405ed9470eac0a
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-08-14 05:48:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remotesensing@210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteSensing/R/vi.R
+++ pkg/RemoteSensing/R/vi.R
@@ -1,25 +1,24 @@
 # Author: Yann Chemin
-# IRRI
 # License GPL3
 # Version 1, October 2008
 
 
-arvi<-function(red,nir,blue) #ARVI: Atmospheric Resistant Vegetation Index
-{
+arvi <- function(red,nir,blue) {
+#ARVI: Atmospheric Resistant Vegetation Index
 # ARVI is resistant to atmospheric effects (in comparison to the NDVI) and is accomplished by a self correcting process for the atmospheric effect in the red nel, using the difference in the radiance between the blue and the red nels. (Kaufman and Tanre 1996) 
 	result <- (nir - (2 * red - blue)) / (nir + (2 * red - blue))
 	return(result)
 }
 
-dvi<-function(red, nir) #DVI: Difference Vegetation Index
-{
+dvi<-function(red, nir) {
+#DVI: Difference Vegetation Index
 	result <- (nir - red)
 	return(result)
 }
 
 
-evi<-function(blue, red, nir) #EVI: Enhanced Vegetation Index 
-{
+evi<-function(blue, red, nir) {
+#EVI: Enhanced Vegetation Index 
 # Huete A.R., Liu H.Q., Batchily K., vanLeeuwen W. (1997)
 # A comparison of vegetation indices global set of TM images for EOS-MODIS
 # Remote Sensing of Environment, 59:440-451.
@@ -31,60 +30,51 @@ evi<-function(blue, red, nir) #EVI: Enhanced Vegetation Index
 } 
 
 
-gari<-function(red, nir, blue, green) #GARI: green atmospherically resistant vegetation index 
-{
+gari<-function(red, nir, blue, green) {
+#GARI: green atmospherically resistant vegetation index 
 	result <- (nir - (green - (blue - red))) / (nir - (green - (blue - red)))
 	return(result)
 }
 
 
-gemi<-function(red,nir) #GEMI: Global Environmental Monitoring Index
-{
+gemi<-function(red,nir) {
+#GEMI: Global Environmental Monitoring Index
 	result <- (((2 * ((nir * nir) - (red * red)) + 1.5 * nir + 0.5 * red) / (nir + red + 0.5)) * (1 - 0.25 * (2 * ((nir * nir) - (red * red)) + 1.5 * nir + 0.5 *  red) /  (nir + red + 0.5))) - ((red - 0.125) / (1 - red))
 	return(result)
 }
 
 
-gvi<-function(blue, green, red, nir, ch5, ch7) #Green Vegetation Index
-{
+gvi<-function(blue, green, red, nir, ch5, ch7) {
+#Green Vegetation Index
 	result <- (-0.2848 * blue - 0.2435 * green - 0.5436 * red + 0.7243 * nir + 0.0840 * ch5 - 0.1800 * ch7)
 	return(result)
 }
 
 
-ipvi<-function(red, nir) #IPVI: Infrared Percentage Vegetation Index
-{
+ipvi<-function(red, nir) {
+#IPVI: Infrared Percentage Vegetation Index
 	result <- (nir) / (nir + red)
 	return(result)
 }
 
 
-msavi2<-function(red, nir) #MSAVI2: second Modified Soil Adjusted Vegetation Index
-{
+msavi2<-function(red, nir) {
+#MSAVI2: second Modified Soil Adjusted Vegetation Index
 	result <- (1 / 2) * (2 * (nir + 1) - sqrt((2 * nir + 1) * (2 * nir + 1)) - (8 * (nir - red)))
 	return(result)
 }
 
 
-msavi<-function(red, nir) #MSAVI: Modified Soil Adjusted Vegetation Index
-{
+msavi<-function(red, nir) {
+#MSAVI: Modified Soil Adjusted Vegetation Index
 # MSAVI = s(NIR-s*red-a) / (a*NIR+red-a*s+X*(1+s*s))     
 # where a is the soil line intercept, s is the soil line slope, and X  is an adjustment factor which is set to minimize soil noise (0.08 in original papers).
 	result <- (1 / 2) * (2 * (nir + 1) - sqrt((2 * nir + 1) * (2 * nir + 1)) - (8 * (nir - red)))
 	return(result)
 }
 
-nddi <- function(NDVI, NDWI) # NDDI: Normalized Difference Drought Index
-{
-	result<- (NDVI - NDWI) / (NDVI + NDWI)
-	result[is.infinite(result)] <- NA
-	result[result < 0] <- 0
-	result[result > 2] <- 2
-	return(result)
-}
-
-ndvi<-function(red, nir) #NDVI: Normalized Difference Vegetation Index
-{
+ndvi<-function(red, nir) {
+#NDVI: Normalized Difference Vegetation Index
 	result<- (nir - red) / (nir + red)
 	result[is.infinite(result)] <- NA
 	result[result < -1] <- -1
@@ -92,17 +82,9 @@ ndvi<-function(red, nir) #NDVI: Normalized Difference Vegetation Index
 	return(result)
 }
 
-ndwi <- function(nir, swir2) # NDWI: Normalized Difference Water Index
-{
-	result<- (nir - swir2) / (nir + swir2)
-	result[is.infinite(result)] <- NA
-	result[result < -1] <- -1
-	result[result > 1] <- 1
-	return(result)
-}
 
-pvi<-function(red, nir) #PVI: Perpendicular Vegetation Index 
-{
+pvi <- function(red, nir) {
+#PVI: Perpendicular Vegetation Index 
 # PVI = sin(a)NIR-cos(a)red
 # for a  isovegetation lines (lines of equal vegetation) would all be parallel to the soil line therefore a=1
 	result <- (sin(1) * nir) / (cos(1) * red)
@@ -110,22 +92,22 @@ pvi<-function(red, nir) #PVI: Perpendicular Vegetation Index
 }
 
  
-savi<-function(red, nir) #SAVI: Soil Adjusted Vegetation Index
-{
+savi <- function(red, nir) {
+#SAVI: Soil Adjusted Vegetation Index
 	result <- ((1 + 0.5) * (nir - red)) / (nir + red + 0.5)
 	return(result)
 }
 
 
-sr<-function(red, nir) #SR: Simple Vegetation ratio
-{
+sr <- function(red, nir) {
+#SR: Simple Vegetation ratio
 	result <- (nir / red)
 	return(result)
 }
 
 
-vari<-function(blue, green, red) #VARI: Visible Atmospherically Resistant Index
-{
+vari <- function(blue, green, red) {
+#VARI: Visible Atmospherically Resistant Index
 # VARI is the Visible Atmospherically Resistant Index, it was 
 # designed to introduce an atmospheric self-correction 
 # Gitelson A.A., Kaufman Y.J., Stark R., Rundquist D., 2002.
@@ -135,15 +117,10 @@ vari<-function(blue, green, red) #VARI: Visible Atmospherically Resistant Index
 	return(result)
 }
 
-wdvi<-function(red, nir, slslope = 1) #WDVI: Weighted Difference Vegetation Index
-{
+wdvi<-function(red, nir, slslope = 1) {
+#WDVI: Weighted Difference Vegetation Index
 	slslope #slope of soil line
 	result <- (nir - slslope * red)
 	return(result)
 }
 
-# DROUGHT where drought = 2, no drought=1
-drought <- function(NDVI, NDWI) {
-	res <- ((NDVI < 0.5 & NDWI < 0.3)*2) + ((NDVI > 0.6 & NDWI > 0.4)*1)
-	return(res)
-	}
--- pkg/RemoteSensing/R/water.R
+++ pkg/RemoteSensing/R/water.R
@@ -4,9 +4,36 @@
 # Version 2, March 2009
 
 
-lswi<-function(nir, swir)
+nddi <- function(ndvi, ndwi) {
+# NDDI: Normalized Difference Drought Index {
+	result<- (ndvi - ndwi) / (ndvi + ndwi)
+	result[is.infinite(result)] <- NA
+	result[result < 0] <- 0
+	result[result > 2] <- 2
+	return(result)
+}
+
+
+drought <- function(ndvi, ndwi) {
+# DROUGHT where drought = 1, no drought=0
+	res <- ((ndvi < 0.5 & ndwi < 0.3)*2) + ((ndvi > 0.6 & ndwi > 0.4)*1) - 1
+	return(! res)
+}
+
+
+
+ndwi <- function(nir, swir2) {
+# NDWI: Normalized Difference Water Index
+	result<- (nir - swir2) / (nir + swir2)
+	result[is.infinite(result)] <- NA
+	result[result < -1] <- -1
+	result[result > 1] <- 1
+	return(result)
+}
+
+
+lswi<-function(nir, swir) {
  #LSWI: Land Surface Water Index
-{
 	result <- (nir - swir) / (nir + swir)
 	result[is.infinite(result)] <- NA
 	result[result < -1] <- -1
@@ -15,14 +42,13 @@ lswi<-function(nir, swir)
 }
 
 
-water<-function(ndvi, albedo) 
+water<-function(ndvi, albedo) {
  #water: generic water mapping tool
-{
 	return( (ndvi < 0.1) & (albedo < 0.1) )
 }
 
 
-waterModis<-function(ndvi, band7)
+waterModis<-function(ndvi, band7) {
  #water.modis: Terra-MODIS water mapping tool
  #Xiao X., Boles S., Liu J., Zhuang D., Frokling S., Li C., Salas W., Moore III B. (2005). 
  #Mapping paddy rice agriculture in southern China using multi-temporal MODIS images. 
@@ -32,7 +58,7 @@ waterModis<-function(ndvi, band7)
  #Prototyping a global algorithm for systematic fire-affected
  #area mapping using MODIS time series data. 
  #Remote Sensing of Environment 97:137-162.
-{
+ 
 	result<- ndvi * band7
 	return( (ndvi < 0.1) & (band7 < 0.04) )	
 	

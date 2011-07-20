a2bdf5eeeef60631df0961434a75dc7acb7f1a8f
jaunario (Jorrel Khalil Aunario) <jaunario@gmail.com>
2011-07-20 10:49:50 +0000

    generalized function, applicable to processed modis outputs in RiceMap
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remotesensing@230 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteSensing/R/modisFiles.R
+++ pkg/RemoteSensing/R/modisFiles.R
@@ -3,7 +3,7 @@
 # Version 0,1
 # Licence GPL v3
 
-
+# TODO: fully integrate dir parameters, i.e. pattern 
 modisFiles <- function(sep="\\.", modisinfo=c('product', 'acqdate', 'zone', 'version', 'proddate', 'band', 'format'), format="GTiff",...) {
     
     filename <- dir(..., pattern=formatExt(format))    
@@ -11,15 +11,15 @@ modisFiles <- function(sep="\\.", modisinfo=c('product', 'acqdate', 'zone', 'ver
     info <- sub(".hdf","",basename(filename))
 	
 	x <- unlist(strsplit(info, sep))
-	m <- as.data.frame(matrix(x, ncol=length(modisinfo), byrow=TRUE))
+	m <- as.data.frame(matrix(x, ncol=length(modisinfo), byrow=TRUE), stringsAsFactors=FALSE)
 	if (ncol(m) != length(modisinfo)) { 
         stop('oops, non standard filenames found') 
     }
     colnames(m) <- modisinfo
     year <- as.numeric(substr(m$acqdate,2,5))
     doy <- as.numeric(substr(m$acqdate,6,8))
-	m <- cbind(filename,year, doy, as.data.frame(m))
-	m$band <- sub("sur_refl_","",m$band)
+	m <- cbind(filename,year, doy, m, stringsAsFactors=FALSE)
+	m$band <- as.character(sub("sur_refl_","",m$band))
 	return(m)
 }
 

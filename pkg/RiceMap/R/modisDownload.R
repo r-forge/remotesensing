6ad60633de5966b6e7ac8b7abea1ddadf824e621
jaunario (Jorrel Khalil Aunario) <jaunario@gmail.com>
2017-07-05 03:00:19 +0000

    Removed unnecessary URL checks that just adds to latency.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remotesensing@288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RiceMap/R/modisDownload.R
+++ pkg/RiceMap/R/modisDownload.R
@@ -7,6 +7,8 @@ dl.fast <- 0
 dl.smart <- 1
 dl.renew <- 2
 
+modprods <- read.csv(system.file("modis.products.ref.csv", package="RiceMap"), stringsAsFactors=FALSE)
+
 modis.integrity <- function(localfile, xml){
 	cksumver <- Sys.which("cksum")	
 	if (cksumver==""){
@@ -25,29 +27,17 @@ modis.integrity <- function(localfile, xml){
 	return(cksum==chk)
 }
 
-modis.download <- function(tile, years, doy=NULL, product="MOD09A1", prod.ver=5, savedir=getwd(), modis.site="http://e4ftl01.cr.usgs.gov/", dl.mode=dl.smart, checkurl=TRUE, integrity=TRUE, skip.exists=TRUE, verbose=TRUE, ...){
-	modprods <- read.csv(system.file("modis.products.ref.csv", package="RiceMap"), stringsAsFactors=FALSE)
-	prod.info <- modprods[grep(product,modprods$ShortName),]
-	modis.site <- paste(modis.site,"MO", switch(prod.info$Platform,Aqua="LA",Terra="LT",Combined="TA"),"/",sep="")
-	
-	message("Determining correct product URL...")
-	modis.page <- unlist(strsplit(getURL(modis.site, dirlistonly=TRUE),"\n"))
-	modis.page <- modis.page[grep(paste(product,sprintf(paste("%03d",sep=""),prod.ver),sep="."),modis.page)]
-	link.st <- regexpr(paste(">",product,".*./",sep=""), modis.page)
-	link.en <- regexpr("</a>", modis.page)
-	
+modis.download <- function(tile, years, doy=NULL, product="MOD09A1", prod.ver=6, savedir=NULL, modis.site="http://e4ftl01.cr.usgs.gov/", dl.mode=dl.smart, integrity=TRUE, skip.exists=TRUE, verbose=TRUE, ...){
 	#Initialize required objects
-	if (!force.directories(savedir,recursive=TRUE)){ # Ensure the path exists on disk
+	if (is.null(savedir)){
+		dirs <- apply(expand.grid(product, tiles, stringsAsFactors = FALSE), FUN = paste, MARGIN = 1, collapse="/")
+		lapply(dirs, FUN=force.directories,recursive=TRUE)
+	} else if (!force.directories(savedir,recursive=TRUE)){ # Ensure the path exists on disk
 		stop("Unable to create save directory. Kindly ensure you have the necessary permissions to use \n", savedir)
 	}
 	
-	validff <- validFolders()	
-	result <- vector() # Empty vector will contain full filename
-	
-	
 	if (dl.mode==dl.fast) {
 		skip.exists <- TRUE
-		checkurl <- FALSE
 		integrity <- FALSE
 	} else if (dl.mode==dl.smart) {
 		integrity <- TRUE
@@ -55,121 +45,104 @@ modis.download <- function(tile, years, doy=NULL, product="MOD09A1", prod.ver=5,
 		skip.exists <- FALSE			
 	}
 	
-	for (i in 1:length(years)){
-		# Generate ACQDATE based on year and doy
-		if(is.null(doy)|is.na(doy)){
-			tim.gran <- paste("t",gsub(" ", "", prod.info$Temporal.Granularity),sep="")
-			acqdates <- switch(tim.gran,
-					t4day=format(as.Date(paste(years[i],seq(from=1,to=365, by=4)), "%Y %j"), "%Y.%m.%d"), 
-					t8day=format(as.Date(paste(years[i],seq(from=1,to=365, by=8)), "%Y %j"), "%Y.%m.%d"), 
-					t16day=format(as.Date(paste(years[i],ifelse(rep(prod.info$Platform,23)=="Aqua",seq(from=9,to=365, by=16),seq(from=1,to=365, by=16))), "%Y %j"), "%Y.%m.%d"), 
-					tYearly=format(as.Date(paste(years[i], 1), "%Y %j"), "%Y.%m.%d"),
-					tmonthly=format(as.Date(paste(years[i], 1:12,1), "%Y %m %d"), "%Y.%m.%d"),
-					tdaily=format(as.Date(paste(years[i], 1:365), "%Y %j"), "%Y.%m.%d"), NA
-			)		
-			
-		} else {
-			acqdates <- format(as.Date(paste(years[i], doy), "%Y %j"), "%Y.%m.%d")	
-		}	
+	result <- vector()
+	
+	for (pr in product){
+		prod.info <- modprods[grep(pr,modprods$ShortName),]
 		
-		if(sum(is.na(acqdates))>0){
-			stop("Unsupported product ", product,". Kindly contact the developer.")
+		if(!grepl("day", prod.info$Temporal.Granularity)){
+			warning("Unsupported product ", product,". Kindly contact the developer.")
+			next
+		}
+		prod.site <- paste(modis.site, "MO", switch(prod.info$Platform, Aqua="LA", Terra="LT", Combined="TA"), "/", paste(pr,sprintf(paste("%03d",sep=""),prod.ver),sep="."),sep="")
+		if (is.null(doy)){
+			tim.gran <- paste("t",gsub(" ", "", prod.info$Temporal.Granularity),sep="")
+			doy <- switch(tim.gran, t4day=seq(from=1,to=365, by=4), t8day=seq(from=1,to=365, by=8), t16day=ifelse(rep(prod.info$Platform,23)=="Aqua", seq(from=9,to=365, by=16), seq(from=1,to=365, by=16)))				
 		}
 		
-		#subfolders <- as.Date(validFolders()[1], "%Y.%m.%d")
-		
-		for (j in 1:length(acqdates)){
-			if (!acqdates[j] %in% validff){
-				if (verbose) message(acqdates[j], " is not a valid subfolder in modis ftp site. Skipping...", appendLF=TRUE)
-				next
-			}
-			
-			product.site <- paste(modis.site, substr(modis.page, link.st[1]+1,link.en-1), acqdates[j], "/", sep="") # MODIS FTP URL for specified product
-						
-			if (checkurl){
-				if (verbose) message("Checking if ", acqdates[j], " is available in ", dirname(product.site), appendLF=TRUE)
-				if(!url.exists(product.site)) {
-					message(product.site, "does not exist. Skipping.", appendLF=TRUE)
-					next
-				}
-			} 
-			if (verbose) message("Acquiring file list in ", product.site, appendLF=TRUE)		
-			
-			# get list of files in product.site 
-			files <- getURL(product.site, dirlistonly=TRUE)	
-			# parse list by line
-			files <- unlist(strsplit(files,"\n")) 
-			
-			# if product.site has files from specified tile
-			if (length(files)>0){
-				# get hdf filename for specified tile 
-				tilefiles <- gsub("\r", "", files[grep(paste(tile,"hdf",sep=".*."),files)])
-								
-				# if tile is found on product.site
-				if (length(tilefiles)==2){ 
-					# extract filenames from html
-					hdffile <- substr(tilefiles[1], regexpr(product,tilefiles[1]), regexpr("hdf",tilefiles[1])+2)
-					xmlfile <- substr(tilefiles[2], regexpr(product,tilefiles[2]), regexpr("xml",tilefiles[2])+2)
+		for (yr in years) {
+			for (d in doy){
+				date.site <- paste(prod.site, "/",	format(as.Date(paste(yr, d), "%Y %j"), "%Y.%m.%d"), "/", sep="")				
+				if (verbose) message("Acquiring file list in ", date.site, appendLF=TRUE)
+				date.page <- unlist(strsplit(getURL(date.site, dirlistonly=TRUE),"\n"))
+				acqdate <- paste("A",yr,sprintf("%03d",d),sep="")
+				
+				for (tl in tile){
+					if (is.null(savedir)) tile.dir <- paste(pr,tl, sep="/") else tile.dir <- savedir
+					tile.page <- date.page[grep(paste(pr, acqdate, tl, sep="."),date.page)]
+					tile.page <- tile.page[-grep("BROWSE",tile.page)]
+					link.st <- regexpr(paste(">",product,".*./",sep=""), tile.page)
+					link.en <- regexpr("</a>", tile.page)
+					tilefiles <- substr(tile.page, link.st+1,link.en-1)
+					tile.site <- paste(date.site, tilefiles, sep="")
 					
-					if (integrity) {
-					  download.file(paste(product.site, xmlfile, sep=""), destfile=xmlfile, quiet=!verbose, ...)
-					  xml <- readLines(xmlfile)
-					  #xml <- unlist(strsplit(getURL(paste(product.site, xmlfile, sep="")),"\n"))
-					}
-					if (file.exists(paste(savedir,hdffile, sep="/")) & skip.exists) {
-						if (verbose) message(hdffile, " exists locally.", appendLF=TRUE)
-						result <- c(result,paste(savedir,hdffile,sep="/"))
-						next
-						# File already present in local savedir
+					if (length(tilefiles)==2){ 
+						# extract filenames from html
+						hdffile <- tilefiles[1]
+						xmlfile <- tilefiles[2]
 						
-					} else if (file.exists(paste(savedir,hdffile, sep="/")) & integrity){
-						
-						
-						if (verbose) {
-							message(hdffile, " exists locally.", appendLF=TRUE)
-							message("Checking integrity...", appendLF=FALSE)
-						}
-						
-						if(modis.integrity(localfile=paste(savedir,hdffile, sep="/"),xml=xml)) {
-							message(" SUCCESS!", appendLF=TRUE)
+						if (file.exists(paste(tile.dir, hdffile, sep="/")) & skip.exists) {
+							if (verbose) message(hdffile, " exists locally.", appendLF=TRUE)
 							result <- c(result,paste(savedir,hdffile,sep="/"))
 							next
-						} else {
-							#if (verbose) message("Downloading ", product.site, hdffile, appendLF=TRUE)		
-							#hdf <- download.file(paste(product.site, hdffile, sep=""), destfile=paste(savedir,hdffile, sep="/"), method='internal', mode='wb',quiet=!verbose)
+							# File already present in local savedir	
+						} 
+						
+						if (integrity) {
+							download.file(tile.site[2], destfile=xmlfile, ...)
+							xml <- readLines(xmlfile)
+							file.remove(xmlfile)
+							#xml <- unlist(strsplit(getURL(paste(product.site, xmlfile, sep="")),"\n"))
+						}
+						
+						if (file.exists(paste(tile.dir, hdffile, sep="/")) & integrity){
+							if (verbose) {
+								message(hdffile, " exists locally.", appendLF=TRUE)
+								message("Checking integrity...", appendLF=FALSE)
+							}
 							
-							message("FAILED. Removing old file.", appendLF=TRUE)
-							unlink(paste(savedir,hdffile, sep="/")) 
+							if(modis.integrity(localfile=paste(tile.dir, hdffile, sep="/"),xml=xml)) {
+								message(" SUCCESS!", appendLF=TRUE)
+								result <- c(result,paste(savedir,hdffile,sep="/"))
+								next
+							} else {
+								#if (verbose) message("Downloading ", product.site, hdffile, appendLF=TRUE)		
+								#hdf <- download.file(paste(product.site, hdffile, sep=""), destfile=paste(savedir,hdffile, sep="/"), method='internal', mode='wb',quiet=!verbose)								
+								message("FAILED. Removing old file.", appendLF=TRUE)
+								file.remove(paste(tile.dir, hdffile, sep="/")) 
+							}
+						} else if (file.exists(paste(tile.dir, hdffile, sep="/")) & !skip.exists){
+							message("Removing old file.", appendLF=TRUE)
+							file.remove(paste(savedir,hdffile, sep="/"))
 						}
-					} else if (file.exists(paste(savedir,hdffile, sep="/")) ){
-						message("Removing old file.", appendLF=TRUE)
-						unlink(paste(savedir,hdffile, sep="/"))
+						
+						
+						# File not yet downloaded - attempt to get it!
+						if (verbose) message("Downloading ", hdffile, appendLF=TRUE)		
+						hdf <- download.file(tile.site[1], destfile=paste(tile.dir, hdffile, sep="/"), ...)
+						
+						# check integrity
+						if (integrity){
+							if (verbose) message("Checking integrity...", appendLF=FALSE)
+							# Verify successful download
+							if(modis.integrity(localfile=paste(tile.dir, hdffile, sep="/"),xml=xml)) {
+								message(" SUCCESS!", appendLF=TRUE)
+								result <- c(result,paste(tile.dir, hdffile, sep="/"))
+								next
+							} else {
+								message("FAILED. Try to redownload later.", appendLF=TRUE)
+								file.remove(paste(tile.dir, hdffile, sep="/")) 
+							}
+						}					
+					} else {
+						message(tile, " not found in ", prod.site, appendLF=TRUE) 
 					}
 					
-					
-					# File not yet downloaded - attempt to get it!
-					if (verbose) message("Downloading ", product.site, hdffile, appendLF=TRUE)		
-					hdf <- download.file(paste(product.site, hdffile, sep=""), destfile=paste(savedir,hdffile, sep="/"), quiet=!verbose, ...)
-					
-					# check integrity
-					if (integrity){
-						if (verbose) message("Checking integrity...", appendLF=FALSE)
-						# Verify successful download
-						if(modis.integrity(localfile=paste(savedir,hdffile, sep="/"),xml=xml)) {
-							message(" SUCCESS!", appendLF=TRUE)
-							result <- c(result,paste(savedir,hdffile,sep="/"))
-							next
-						} else {
-							message("FAILED. Try to redownload later.", appendLF=TRUE)
-							unlink(paste(savedir,hdffile, sep="/")) 
-						}
-					}					
-				} else {
-					message(tile, " not found in ", product.site, appendLF=TRUE) 
-				}
-			}				
-		}		
-	} 
+				}			
+			}
+		}
+	}
+	
 	return(result)
 }
 
@@ -199,7 +172,7 @@ modis.hdf2tif <- function(hdffile, outdir=getwd(), MRT_HOME=Sys.getenv("MRT_HOME
 			
 			filename <- paste(MRT, "/modisconfig.prm", sep="")
 			mrtconfig <- c(paste('INPUT_FILENAME = ', hdffile, sep=""), 
-					'SPECTRAL_SUBSET = ( ', paste(spectral_subset, collapse=" "),' )',
+					paste('SPECTRAL_SUBSET = ( ', paste(spectral_subset, collapse=" "),' )', sep=""),
 					paste('OUTPUT_FILENAME = ', outdir,"/", sub(".hdf","",basename(hdffile)),'.tif', sep=""), 
 					paste('RESAMPLING_TYPE =', resampling_type), 
 					paste('OUTPUT_PROJECTION_TYPE =', toupper(output_projection)),
